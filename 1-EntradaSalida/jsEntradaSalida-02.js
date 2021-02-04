2baa369af40204eef8658fc42b68af291207ec27
Lucas javier Altamirano <altamiranolucasjavier@gmail.com>
2021-02-03 22:45:12 -0300

    clase uno de progrmacion

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,13 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	var nombre 
 	alert("ok");
+	var nombre = prompt("Please enter your name", "Harry Potter");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! How are you today?";
+
 }
 
